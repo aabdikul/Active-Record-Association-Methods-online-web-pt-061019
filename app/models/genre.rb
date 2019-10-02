0825d7858d8a129d324a654df72199805a340362
Aida Abdikulova <abdikulova@gmail.com>
2019-10-02 05:37:23 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,17 +2,5 @@ class Genre < ActiveRecord::Base
   has_many :songs
   has_many :artists, through: :songs
 
-  def song_count
-    self.songs.size
-  end
 
-  def artist_count
-    self.artists.size
-  end
-
-  def all_artist_names
-    self.artists.map do |value|
-      value.name
-    end
-  end
 end
