9ddf69fd18bba949adf1a76942a141f85ddc90bf
Aida Abdikulova <abdikulova@gmail.com>
2019-10-02 05:34:56 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,15 +2,4 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
-  def get_genre_of_first_song
-    self.genres.first
-  end
-
-  def song_count
-    self.songs.size
-  end
-
-  def genre_count
-    self.genres.size
-  end
 end
